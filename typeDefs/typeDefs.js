ed79a4fb356d3c58c78f0b50e6cfc73af423fcba
Hedi <hedi9507@gmail.com>
2018-10-16 09:49:02 +0200

    create type defs

--- typeDefs/typeDefs.js
+++ typeDefs/typeDefs.js
@@ -3,8 +3,37 @@
 const { gql } = require('apollo-server')
 
 const TypeDefs = gql`
+  type List = {
+    id: String
+    title: String
+    where: String
+    when: Data
+    to: Date
+    notes: String
+  }
+
+  type ListInput = {
+    title: String
+    where: String
+    when: Date
+    to: Date
+    notes: String
+  }
+
   type Query {
-    hello: String
+    hello: String,
+    getItems: [List]
+    getOneItem(id: String): List
+  }
+
+  type Delete {
+    ok
+  }
+
+  type Mutation {
+    createOne(id: String, input: ListInput): List
+    deleteOne(id: String, input: ListInput): Delete
+    updateOne(id: String, input: ListInput): List
   }
 `
 module.exports = TypeDefs
