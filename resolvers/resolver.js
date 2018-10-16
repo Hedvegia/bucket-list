d42fafa63942cc894fb82fc26d2f9b040eb60980
Hedi <hedi9507@gmail.com>
2018-10-16 13:23:11 +0200

    refact

--- resolvers/resolver.js
+++ resolvers/resolver.js
@@ -6,11 +6,11 @@ const resolvers = {
   Query: {
     hello: () => 'Hello',
     getItems: () => get(),
-    getOneItem: (root, args) => getOne(args)
+    getOneItem: (root, { id: _id }) => getOne({ id: _id })
   },
   Mutation: {
     createOne: (root, args) => create(args),
-    deleteOne: (root, args) => deleteOne(args),
+    deleteOne: (root, { id: _id }) => deleteOne({ id: _id }),
     updateOne: (root, args) => update(args)
   }
 }
